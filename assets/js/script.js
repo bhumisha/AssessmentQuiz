5de3b02e33f7830309866fcab51ed407ac3df460
Bhumisha Dave <bhumishadave@Bhumishas-MBP.hsd1.ca.comcast.net>
2020-12-20 03:21:08 -0800

    Move Data to separate file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,75 +1,93 @@
 
-var questions = [{
-                    q:"Which one is wrong JavaScript Data Types?",
-                    option:{
-                        1:"Number",
-                        2:"String",
-                        3:"Boolean",
-                        4:"Long"
-                    },
-                    correctAnswer:"4"
-                },
-                {
-                    q:"Which function joins all elements of an array into a string??",
-                    option:{
-                        1:"pop()",
-                        2:"join()",
-                        3:"append()",
-                        4:"concat()"
-                    },
-                    correctAnswer:"2"
-                },
-                {
-                    q:"Which built-in method returns the character at the specified index of String?",
-                    option:{
-                        1:"indexOf()",
-                        2:"valueOf()",
-                        3:"charAt()",
-                        4:"substr()"
-                    },
-                    correctAnswer:"3"
-                },
-                {
-                    q:"Which of the following is not a valid JavaScript variable name?",
-                    option:{
-                        1:"Name1",
-                        2:"First_Last",
-                        3:"34Rooms",
-                        4:"None of above"
-                    },
-                    correctAnswer:"3"
-                },
-                {
-                    q:"Inside which HTML tag do we put the JavaScript?",
-                    option:{
-                        1:"<js>",
-                        2:"<scripting>",
-                        3:"<script>",
-                        4:"<javascript>"
-                    },
-                    correctAnswer:"3"
-                },
-                {
-                    q:" _______ statement is used to test a specific condition",
-                    option:{
-                        1:"Select",
-                        2:"If",
-                        3:"Switch",
-                        4:"For"
-                    },
-                    correctAnswer:"2"
-                },
-                {
-                    q:"The external JavaScript file must contain <script> tag. True or False?",
-                    option:{
-                        1:"true",
-                        2:"false"
-                    },
-                    correctAnswer:"2"
-                },
+// var questions = [{
+//                     q:"Which one is wrong JavaScript Data Types?",
+//                     option:{
+//                         1:"Number",
+//                         2:"String",
+//                         3:"Boolean",
+//                         4:"Long"
+//                     },
+//                     correctAnswer:"4"
+//                 },
+//                 {
+//                     q:"Which function joins all elements of an array into a string??",
+//                     option:{
+//                         1:"pop()",
+//                         2:"join()",
+//                         3:"append()",
+//                         4:"concat()"
+//                     },
+//                     correctAnswer:"2"
+//                 },
+//                 {
+//                     q:"Which built-in method returns the character at the specified index of String?",
+//                     option:{
+//                         1:"indexOf()",
+//                         2:"valueOf()",
+//                         3:"charAt()",
+//                         4:"substr()"
+//                     },
+//                     correctAnswer:"3"
+//                 },
+//                 {
+//                     q:"Which of the following is not a valid JavaScript variable name?",
+//                     option:{
+//                         1:"Name1",
+//                         2:"First_Last",
+//                         3:"34Rooms",
+//                         4:"None of above"
+//                     },
+//                     correctAnswer:"3"
+//                 },
+//                 {
+//                     q:"Inside which HTML tag do we put the JavaScript?",
+//                     option:{
+//                         1:"<js>",
+//                         2:"<scripting>",
+//                         3:"<script>",
+//                         4:"<javascript>"
+//                     },
+//                     correctAnswer:"3"
+//                 },
+//                 {
+//                     q:" _______ statement is used to test a specific condition",
+//                     option:{
+//                         1:"Select",
+//                         2:"If",
+//                         3:"Switch",
+//                         4:"For"
+//                     },
+//                     correctAnswer:"2"
+//                 },
+//                 {
+//                     q:"The external JavaScript file must contain <script> tag. True or False?",
+//                     option:{
+//                         1:"true",
+//                         2:"false"
+//                     },
+//                     correctAnswer:"2"
+//                 },
 
-            ];
+//             ];
 
+
+
+function loadJSON(callback) {   
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', './assets/js/data.json', true);
+    xobj.onreadystatechange = function () {
+      if (xobj.readyState == 4 && xobj.status == "200") {
+        callback(JSON.parse(xobj.responseText));
+      }
+    };  
+    xobj.send(null);  
+}
+var questions;
+loadJSON(function(json) {
+    console.log(json); // this will log out the json object
+    questions = json; 
+});
 var timeLimit =  60;
 var questionIndex=0;
 var userScore =0;
@@ -117,13 +135,12 @@ function createQuestionSection(){
 
     var optionEl = document.createElement("ol");
     optionEl.id="optionEL"
-    // optionEl.setAttribute("type","a");
-    
 
+    var br = document.createElement("br");
     var currentQuestionResult = document.createElement("div");
     currentQuestionResult.id="currentQuestionResult";
     currentQuestionResult.setAttribute("class","showcorrectAnswerDiv");
-
+    currentQuestionResult.appendChild(br);
     answerDivEl.appendChild(optionEl);
     section.appendChild(questionH1El);
     section.appendChild(answerDivEl);
@@ -144,7 +161,7 @@ function startQuestionsQuiz(){
 function createAnswerPanel(){
     document.querySelector("#optionEL").innerHTML="";
     var currentQuestionObj = questions[questionIndex];
-    var currentQuestion = currentQuestionObj.q;
+    var currentQuestion = currentQuestionObj.question;
     var currentquestionOptions = currentQuestionObj.option;
     document.querySelector("#questionH1").textContent = currentQuestion;
     if(currentquestionOptions){
